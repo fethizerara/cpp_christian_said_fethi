cada4f5b74f36876fc48624fcae5930b08362f69
Ravoavy <36300959+chrisrhcm@users.noreply.github.com>
2020-05-22 23:14:53 +0200

    Update Perceptron.cpp

--- Perceptron.cpp
+++ Perceptron.cpp
@@ -7,11 +7,7 @@
 
 //pas fini
 Perceptron::Perceptron(int taille_input, Fonction_activation *f, char label) {
-    fonctionActivation = f;
-    this->label = label;
-    for(int i=0; i<taille_input; i++){
-        poids[i]=0;
-    }
+    
 }
 
 double Perceptron::get_poids(int index) {
